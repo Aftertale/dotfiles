d1eae652de2bed267133696f9d6341e988ecfdea
Dillon McDowell <dillon.mcdowell@updater.com>
2021-11-22 10:49:10 -0800

    feat(nvim): add gitsigns

--- nvim/lua/aftertale/plugins.lua
+++ nvim/lua/aftertale/plugins.lua
@@ -1,14 +1,34 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 return require('packer').startup(function(use)
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
-  use 'tpope/vim-fugitive'
+  use 'wbthomason/packer.nvim'
+  -- Git
+  use {
+    { 'tpope/vim-fugitive', cmd = { 'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull' }, disable = true },
+    {
+      'lewis6991/gitsigns.nvim',
+      requires = { 'nvim-lua/plenary.nvim' },
+      config = [[require('config.gitsigns')]],
+    },
+    { 'TimUntersberger/neogit', cmd = 'Neogit', config = [[require('config.neogit')]] },
+  }
+  
+  -- Highlights
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = {
+      'nvim-treesitter/nvim-treesitter-refactor',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    run = ':TSUpdate',
+  }
+  
+  -- Marks
+  use { 'kshenoy/vim-signature', config = [[require('config.signature')]], disable = true }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
